8968e4737ae3f6edc22c7103111534d64a3daf74
Alessandro <marzucoalessandro1@gmail.com>
2020-11-12 16:20:55 +0100

    esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var name = prompt("quale è il tuo nome?");
+
+var surname = prompt("quale è il tuo cognome?");
+
+var color = prompt("quale è il tuo colore preferito?");
+
+document.getElementById('psw').innerHTML = name + surname + color + "19";
